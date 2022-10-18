056d345a833f2cd8201761a8e675003844155f1e
Ales Bukovsky <ales.bukovsky@gmail.com>
2022-10-18 18:11:09 -0400

    feat: publish local data copy for UI development

--- publish.sh
+++ publish.sh
@@ -7,3 +7,4 @@ rm -f $OUT
 _sql -c "\copy (SELECT JSON_BUILD_OBJECT('asOf', (SELECT value FROM metas WHERE key = 'last_update'), 'homes', (SELECT ARRAY_AGG(h) FROM (SELECT * FROM homes ORDER BY price ASC) as h))) to '$OUT'"
 
 scp $OUT $PI_USER@$PI_HOST:$PI_WWW
+cp $OUT ./src/main/web
