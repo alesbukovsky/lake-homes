d5e3b174f64f050ef7cbe0cc669d7cd624e39dfe
Ales Bukovsky <ales.bukovsky@gmail.com>
2022-11-14 09:54:58 -0500

    feat: change HTTP server deployment directory

--- _lib.sh
+++ _lib.sh
@@ -3,9 +3,9 @@ export DB_PORT=5599
 export DB_SCHEMA=lake-homes
 export DB_USER=hombre
 
-export PI_HOST=192.168.1.99
+export PI_HOST=berry.local
 export PI_USER=pi
-export PI_WWW="/home/$PI_USER/Dev/www"
+export PI_WWW="/home/$PI_USER/www/homes"
 
 _sql() {
     psql -h $DB_HOST -p $DB_PORT -d $DB_SCHEMA -U $DB_USER -v ON_ERROR_STOP=1 $1 "$2"
